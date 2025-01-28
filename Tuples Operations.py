5c33fd1fa2439696c49b1e1459ce175639522c66
Saifan594 <saifanhasan72@gmail.com>
2025-01-28 18:41:10 +0600

    first commit

---
+++ Tuples Operations.py
@@ -0,0 +1,15 @@
+t1 = ("tuple", False, 3.2, 1)
+print(t1)
+
+t1 = t1 + ("saifan",)
+t1 = t1 + (9,)
+
+print(t1)
+
+tuple1 = (50, 10, 60, 70, 50)
+print(tuple1.count(50))
+
+tuplex = (2, 4, 3, 5, 4, 6, 7, 8, 6, 1)
+slice = tuplex[3 : 8]
+
+print(slice)
